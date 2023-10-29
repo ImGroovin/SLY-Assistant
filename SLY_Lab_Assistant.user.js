9268390767ef31b0a87c76388b5df02815796398
Groove <64545918+ImGroovin@users.noreply.github.com>
2023-10-28 22:25:02 -0400

    v0.3.5 hotfix 1
    Adding additional logic to avoid unintended movement to 0,0 (reintroduced in 0.3.5)

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -1666,7 +1666,7 @@
             let warpCooldownFinished = 0;
             let fleetAcctInfo = await solanaConnection.getAccountInfo(userFleets[i].publicKey);
             let [fleetState, extra] = getFleetState(fleetAcctInfo);
-            if (fleetState == 'Idle' && extra && moveDist && moveX !== null && moveX !== '' && moveY != null && moveY !== '') {
+            if (fleetState == 'Idle' && extra.length > 1 && moveDist && moveX !== null && moveX !== '' && moveY != null && moveY !== '') {
                 if (extra[0] !== moveX || extra[1] !== moveY) {
                     let warpCost = calculateWarpFuelBurn(userFleets[i], moveDist);
                     let subwarpCost = calculateSubwarpFuelBurn(userFleets[i], moveDist);
