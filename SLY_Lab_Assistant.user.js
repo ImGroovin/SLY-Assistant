74ed1416985781493ef316a77853eb4d6c6b6c60
Paul Miller <neofox2000@gmail.com>
2024-01-02 07:44:22 -0400

    Added more scanning patterns

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SAGE Lab Assistant Modded
 // @namespace    http://tampermonkey.net/
-// @version      0.4.1.3m
+// @version      0.4.1.4m
 // @description  try to take over the world!
 // @author       SLY w/ Contributions by SkyLove512, anthonyra, niofox
 // @match        https://*.labs.staratlas.com/
@@ -22,6 +22,8 @@
 	let debugLogLevel = 3; //How much console logging you want to see (higher number = more, 0 = none)
 	let extraFuelToDropOffAtTarget = 0; //How much excess fuel to leave at target during transport assignments
 	let transportStopOnError = true; //Should transport fleet stop completely if there's an error (example: not enough resource/fuel/etc.)
+	let scanBlockPattern = 'right'; //Valid patterns: square, ring, up, down, left, right
+	let scanBlockLength = 7; //Length of the line-based patterns (does not apply to square or ring)
 
 	//Used for reading solana data
 	let readRPCs = [
@@ -1939,6 +1941,49 @@
 		updateAssistStatus(fleet);
 	}
 
+	function buildScanBlock(destX, destY) {
+		destX = Number(destX);
+		destY = Number(destY);
+		let scanBlock = [];
+		if (destX == '' || destY == '') return scanBlock;
+
+		if(scanBlockPattern == 'square') {
+			scanBlock.push([destX, destY]);
+			scanBlock.push([destX+1, destY]);
+			scanBlock.push([destX+1, destY+1]);
+			scanBlock.push([destX, destY+1]);
+		}
+		else if(scanBlockPattern == 'ring') {
+			scanBlock.push([destX, destY]);
+			scanBlock.push([destX+1, destY]);
+			scanBlock.push([destX+2, destY]);
+			scanBlock.push([destX+2, destY+1]);
+			scanBlock.push([destX+2, destY+2]);
+			scanBlock.push([destX+1, destY+2]);
+			scanBlock.push([destX, destY+2]);
+			scanBlock.push([destX, destY+1]);
+		}
+		else if(scanBlockPattern == 'up') {
+			for(let i=0; i < scanBlockLength; i++) scanBlock.push([destX, destY + i]);
+			for(let i=0; i < scanBlockLength; i++) scanBlock.push([destX + 1, destY - i]);
+		}
+		else if(scanBlockPattern == 'down') {
+			for(let i=0; i < scanBlockLength; i++) scanBlock.push([destX, destY - i]);
+			for(let i=0; i < scanBlockLength; i++) scanBlock.push([destX + 1, destY + i]);
+		}
+		else if(scanBlockPattern == 'left') {
+			for(let i=0; i < scanBlockLength; i++) scanBlock.push([destX - i, destY]);
+			for(let i=0; i < scanBlockLength; i++) scanBlock.push([destX + i, destY + 1]);
+		}
+		else if(scanBlockPattern == 'right') {
+			for(let i=0; i < scanBlockLength; i++) scanBlock.push([destX + i, destY]);
+			for(let i=0; i < scanBlockLength; i++) scanBlock.push([destX - i, destY + 1]);
+		}
+
+		scanBlockLength = scanBlock.length;
+		return scanBlock;
+	}
+
 	async function saveAssistInput() {
 			let fleetRows = document.querySelectorAll('#assistModal .assist-fleet-row');
 			let scanRows = document.querySelectorAll('#assistModal .assist-scan-row');
@@ -2067,17 +2112,8 @@
 							let fleetSavedData = await GM.getValue(fleetPK, '{}');
 							let fleetParsedData = JSON.parse(fleetSavedData);
 							let fleetMoveTarget = fleetParsedData && fleetParsedData.moveTarget ? fleetParsedData.moveTarget : '';
-							destX = Number(destX);
-							destY = Number(destY);
-							let scanShiftX = destX > 0 ? -1 : 1;
-							let scanShiftY = destY > 0 ? -1 : 1;
-							let scanBlock = [];
-							if (destX !== '' && destY !== '') {
-								scanBlock.push([destX, destY]);
-								scanBlock.push([destX+scanShiftX, destY]);
-								scanBlock.push([destX+scanShiftX, destY+scanShiftY]);
-								scanBlock.push([destX, destY+scanShiftY]);
-							}
+							let scanBlock = buildScanBlock(destX, destY);
+
 							await GM.setValue(fleetPK, `{\"name\": \"${fleetName}\", \"assignment\": \"${fleetAssignment}\", \"mineResource\": \"${fleetMineResource}\", \"dest\": \"${fleetDestCoord}\", \"starbase\": \"${fleetStarbaseCoord}\", \"moveType\": \"${moveType}\", \"subwarpPref\": \"${subwarpPref}\", \"moveTarget\": \"${fleetMoveTarget}\", \"transportResource1\": \"${transportResource1}\", \"transportResource1Perc\": ${transportResource1Perc}, \"transportResource2\": \"${transportResource2}\", \"transportResource2Perc\": ${transportResource2Perc}, \"transportResource3\": \"${transportResource3}\", \"transportResource3Perc\": ${transportResource3Perc}, \"transportResource4\": \"${transportResource4}\", \"transportResource4Perc\": ${transportResource4Perc}, \"transportSBResource1\": \"${transportSBResource1}\", \"transportSBResource1Perc\": ${transportSBResource1Perc}, \"transportSBResource2\": \"${transportSBResource2}\", \"transportSBResource2Perc\": ${transportSBResource2Perc}, \"transportSBResource3\": \"${transportSBResource3}\", \"transportSBResource3Perc\": ${transportSBResource3Perc}, \"transportSBResource4\": \"${transportSBResource4}\", \"transportSBResource4Perc\": ${transportSBResource4Perc}, \"scanBlock\": ${JSON.stringify(scanBlock)}, \"scanMin\": ${scanMin}, \"scanMove\": \"${scanMove}\"}`);
 							userFleets[userFleetIndex].mineResource = fleetMineResource;
 							userFleets[userFleetIndex].destCoord = fleetDestCoord;
@@ -2133,17 +2169,7 @@
 		for (let key in jsonTargets) {
 				let destXStr = jsonTargets[key].sector_target[0].toString().trim();
 				let destYStr = jsonTargets[key].sector_target[1].toString().trim();
-				let destX = Number(destXStr);
-				let destY = Number(destYStr);
-				let scanShiftX = destX > 0 ? -1 : 1;
-				let scanShiftY = destY > 0 ? -1 : 1;
-				let scanBlock = [];
-				if (destX !== '' && destY !== '') {
-						scanBlock.push([destX, destY]);
-						scanBlock.push([destX+scanShiftX, destY]);
-						scanBlock.push([destX+scanShiftX, destY+scanShiftY]);
-						scanBlock.push([destX, destY+scanShiftY]);
-				}
+				let scanBlock = buildScanBlock(destX, destY);
 				let fleetSavedData = await GM.getValue(key, '{}');
 				let fleetParsedData = JSON.parse(fleetSavedData);
 				fleetParsedData.dest = destXStr + ',' + destYStr;
@@ -2415,11 +2441,11 @@
 							let shouldMove = strike && userFleets[i].scanMove;
 							userFleets[i].scanSkipCnt = strike ? userFleets[i].scanSkipCnt + 1 : 0;
 							cLog(1,`${FleetTimeStamp(userFleets[i].label)} ${Math.round(scanCondition)}%${sduFound > 0 ? ` | FOUND: ${sduFound}` : ''}`);
-							let nextMoveIdx = userFleets[i].scanBlockIdx > 2 ? 0 : userFleets[i].scanBlockIdx+1;
+							let nextMoveIdx = userFleets[i].scanBlockIdx > scanBlockLength - 2 ? 0 : userFleets[i].scanBlockIdx+1;
 							userFleets[i].scanBlockIdx = shouldMove ? nextMoveIdx : userFleets[i].scanBlockIdx;
 							userFleets[i].toolCnt = changesTool.postBalance;
 							userFleets[i].sduCnt = changesSDU.postBalance;
-							if (userFleets[i].scanSkipCnt < 4) {
+							if (userFleets[i].scanSkipCnt < scanBlockLength - 1) {
 									let scanDelayMs = userFleets[i].scanCooldown * 1000 + 2000;
 									//Wait at least 1.5 minutes for sector to regen
 									if(sduFound) scanDelayMs = Math.max(scanDelayMs, 90000);
@@ -3118,7 +3144,7 @@
 			if(userFleets[i].scanMove) {
 					cLog(2, `${FleetTimeStamp(userFleets[i].label)} Checking scanBlock`);
 
-					for (let s=0; s < 4; s++) {
+					for (let s=0; s < scanBlockLength - 1; s++) {
 							const testCoords = userFleets[i].scanBlock[s];
 							if (fleetCoords[0] == testCoords[0] && fleetCoords[1] == testCoords[1])
 							{
