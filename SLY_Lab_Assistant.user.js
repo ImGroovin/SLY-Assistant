b01a42cbd91e8146211a07c2f38f656de6364b78
Paul Miller <neofox2000@gmail.com>
2024-02-01 11:42:11 -0400

    Upped version number

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SAGE Lab Assistant Modded
 // @namespace    http://tampermonkey.net/
-// @version      0.4.3m0
+// @version      0.4.3m1
 // @description  try to take over the world!
 // @author       SLY w/ Contributions by SkyLove512, anthonyra, niofox
 // @match        https://*.labs.staratlas.com/
