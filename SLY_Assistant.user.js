5aab9e4988c33288c2df67f49e990355cbb67731
Spartaco17 <160352499+Spartaco17@users.noreply.github.com>
2024-05-26 08:50:47 +0200

    Update SLY_Assistant.user.js

--- SLY_Assistant.user.js
+++ SLY_Assistant.user.js
@@ -6,9 +6,9 @@
 // @author       SLY w/ Contributions by niofox, SkyLove512, anthonyra
 // @match        https://*.based.staratlas.com/
 // @require      https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/anchor-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/buffer-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/bs58-browserified.js
+// @require      https://raw.githubusercontent.com/Spartaco17/SLY-Assistant/main/anchor-browserified.js
+// @require      https://raw.githubusercontent.com/Spartaco17/SLY-Assistant/main/buffer-browserified.js
+// @require      https://raw.githubusercontent.com/Spartaco17/SLY-Assistant/main/bs58-browserified.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=staratlas.com
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -22,7 +22,7 @@
 	let customReadRPCs = [];
 
 	//Used for pushing transactions to solana chain
-	let customWriteRPCs = [];
+	let customWriteRPCs = 'https://distinguished-aged-lake.solana-mainnet.quiknode.pro/216fe5679f89f04ee1aa7ab5dd43f96c9b99b77c/'
 
     let saRPCs = [
         'https://twilight-autumn-diagram.solana-mainnet.quiknode.pro/4fc53d638efd1cc0f80764bc457944bb325d1ff1', //Quicknode
