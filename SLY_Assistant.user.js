da2937a89b679c78bdbf1254f2ae44e35f955b0f
Swift42 <1180176+Swift42@users.noreply.github.com>
2024-08-06 14:14:15 +0200

    Update SLY_Assistant.user.js
    Reduced priority fee for SDU scanning (25% of the full fee)

--- SLY_Assistant.user.js
+++ SLY_Assistant.user.js
@@ -1151,7 +1151,7 @@
 
             updateFleetState(fleet, `Scanning [${TimeToStr(new Date(Date.now()))}]`);
 
-            let txResult = await txSignAndSend(tx, fleet, 'SCAN', 100);
+            let txResult = await txSignAndSend(tx, fleet, 'SCAN', 25);
 
             resolve(txResult);
         });
