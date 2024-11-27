f7e57c402437e4cf118bb786ed0dc9a4dca1a35c
Swift42 <1180176+Swift42@users.noreply.github.com>
2024-11-27 20:31:20 +0100

    Update SLY_Assistant.user.js

--- SLY_Assistant.user.js
+++ SLY_Assistant.user.js
@@ -5461,7 +5461,7 @@
                 } else {
                     materialStr = ': ' + targetRecipe.craftRecipe.name + (userCraft.item != targetRecipe.craftRecipe.name ? ' (' + userCraft.item + ')' : '' );
                 }
-                if(availableCrew < userCraft.crew && targetRecipe && targetRecipe.amountCraftable <= 0) {
+                if(availableCrew < userCraft.crew && ((targetRecipe && targetRecipe.amountCraftable <= 0) || (!targetRecipe)) ) {
                     updateFleetState(userCraft, 'Waiting for crew/material' + materialStr);
                 }
                 else if(availableCrew < userCraft.crew) {
