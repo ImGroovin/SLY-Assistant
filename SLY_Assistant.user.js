6c0887078cb9fecc1a6f6b0c2b5b4753dbda7ae4
Swift42 <1180176+Swift42@users.noreply.github.com>
2024-12-04 00:08:17 +0100

    Update SLY_Assistant.user.js
    Version number increased

--- SLY_Assistant.user.js
+++ SLY_Assistant.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SLY Assistant
 // @namespace    http://tampermonkey.net/
-// @version      0.6.26
+// @version      0.6.27
 // @description  try to take over the world!
 // @author       SLY w/ Contributions by niofox, SkyLove512, anthonyra, [AEP] Valkynen, Risingson, Swift42
 // @match        https://*.based.staratlas.com/
