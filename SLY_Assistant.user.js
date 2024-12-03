3d22f3bc8fa6a11270e21b18b6201d8a9d94e7dd
Swift42 <1180176+Swift42@users.noreply.github.com>
2024-12-04 00:59:36 +0100

    Update SLY_Assistant.user.js
    Now pinned web3.js version 1.95.8, so it is the same version as in Groove's hotfix

--- SLY_Assistant.user.js
+++ SLY_Assistant.user.js
@@ -5,7 +5,7 @@
 // @description  try to take over the world!
 // @author       SLY w/ Contributions by niofox, SkyLove512, anthonyra, [AEP] Valkynen, Risingson, Swift42
 // @match        https://*.based.staratlas.com/
-// @require      https://unpkg.com/@solana/web3.js@1.95.5/lib/index.iife.min.js
+// @require      https://unpkg.com/@solana/web3.js@1.95.8/lib/index.iife.min.js
 // @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/anchor-browserified.js
 // @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/buffer-browserified.js
 // @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/bs58-browserified.js
