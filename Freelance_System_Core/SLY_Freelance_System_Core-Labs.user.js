0ea628dffe9cc41ef3a6d2492ad5aac2ad1da5f1
Groove <64545918+ImGroovin@users.noreply.github.com>
2024-12-03 18:46:46 -0500

    Update SLY_Freelance_System_Core-Labs.user.js

--- Freelance_System_Core/SLY_Freelance_System_Core-Labs.user.js
+++ Freelance_System_Core/SLY_Freelance_System_Core-Labs.user.js
@@ -5,7 +5,7 @@
 // @description  try to take over the world!
 // @author       SLY
 // @match        https://labs.staratlas.com/
-// @require      https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js
+// @require      https://unpkg.com/@solana/web3.js@1.95.8/lib/index.iife.min.js
 // @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/anchor-browserified.js
 // @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/buffer-browserified.js
 // @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/bs58-browserified.js
