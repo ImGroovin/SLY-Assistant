8510d29dbdda0b0e74afe13088e40da489281555
Groove <64545918+ImGroovin@users.noreply.github.com>
2024-12-03 19:00:32 -0500

    Update SLY_Freelance_System_Core-Labs.user.js

--- Freelance_System_Core/SLY_Freelance_System_Core-Labs.user.js
+++ Freelance_System_Core/SLY_Freelance_System_Core-Labs.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [FLY] Freelance System: Core
 // @namespace    http://tampermonkey.net/
-// @version      0.4.2
+// @version      0.4.3
 // @description  try to take over the world!
 // @author       SLY
 // @match        https://labs.staratlas.com/
