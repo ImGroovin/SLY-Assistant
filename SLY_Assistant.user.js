97a7a1eb6dc5106a682dd79e448af1d575d1984f
Swift42 <1180176+Swift42@users.noreply.github.com>
2025-01-31 09:51:52 +0100

    Update SLY_Assistant.user.js
    Hotfix

--- SLY_Assistant.user.js
+++ SLY_Assistant.user.js
@@ -4881,7 +4881,7 @@ async function sendAndConfirmTx(txSerialized, lastValidBlockHeight, txHash, flee
 					//await wait(2000);
 					//userFleets[i].moveTarget = userFleets[i].destCoord;
 				} else {
-					if(fleet.stopping) return;
+					if(userFleets[i].stopping) return;
 					userFleets[i].moveTarget = userFleets[i].starbaseCoord;
 					await handleMineMovement();
 				}
@@ -4889,7 +4889,7 @@ async function sendAndConfirmTx(txSerialized, lastValidBlockHeight, txHash, flee
 
 			//At mining area?
 			else if (fleetCoords[0] == destX && fleetCoords[1] == destY) {
-		if(fleet.stopping) return;
+		if(userFleets[i].stopping) return;
                 fleetCurrentCargo = await solanaReadConnection.getParsedTokenAccountsByOwner(userFleets[i].cargoHold, {programId: tokenProgramPK});
                 cargoCnt = fleetCurrentCargo.value.reduce((n, {account}) => n + account.data.parsed.info.tokenAmount.uiAmount, 0);
                 currentFood = fleetCurrentCargo.value.find(item => item.account.data.parsed.info.mint === sageGameAcct.account.mints.food.toString());
@@ -4911,7 +4911,7 @@ async function sendAndConfirmTx(txSerialized, lastValidBlockHeight, txHash, flee
 
 			//Move to mining area
 			else {
-				if(fleet.stopping) return;
+				if(userFleets[i].stopping) return;
 				userFleets[i].moveTarget = userFleets[i].destCoord;
 				await handleMineMovement();
 			}
@@ -5102,7 +5102,7 @@ async function sendAndConfirmTx(txSerialized, lastValidBlockHeight, txHash, flee
                 cLog(3,`${FleetTimeStamp(userFleets[i].label)} userFleets[i]: `, userFleets[i]);
             }
 
-            if(fleet.stopping) return;
+            if(userFleets[i].stopping) return;
 
             if (userFleets[i].moveTarget !== '') {
                 const targetX = userFleets[i].moveTarget.split(',').length > 1 ? userFleets[i].moveTarget.split(',')[0].trim() : '';
